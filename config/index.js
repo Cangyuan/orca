29658b8ff62db73495397a1f2d9c81f1bf02c4d8
cangyuan <1339918746@qq.com>
2017-12-08 15:25:19 +0800

    test

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '',
+    assetsPublicPath: '/',
     proxyTable: {
       '/api/v1/**': {
         target: 'https://cnodejs.org', // 你接口的域名
